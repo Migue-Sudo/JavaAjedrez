e0c54115a466df6023eab41ceffb185d25000df1
javie <javie@192.168.1.132>
2024-02-06 18:57:06 +0100

    Base1.2

---
+++ JavaAjedrez/src/javajedrez/Estado.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javajedrez;
+
+/**
+ *
+ * @author javie
+ */
+public class Estado {
+    static final int RELLENO = 7;
+    static final int VACIO = 0;
+    static final int PEON = 1;
+    static final int CABALLO = 2;
+    static final int ALFIL = 3;
+    static final int TORREO = 4;
+    static final int REINA = 5;
+    static final int REY = 9;
+
+    public static int getRELLENO() {
+        return RELLENO;
+    }
+
+    public static int getVACIO() {
+        return VACIO;
+    }
+
+    public static int getPEON() {
+        return PEON;
+    }
+
+    public static int getCABALLO() {
+        return CABALLO;
+    }
+
+    public static int getALFIL() {
+        return ALFIL;
+    }
+
+    public static int getTORREO() {
+        return TORREO;
+    }
+
+    public static int getREINA() {
+        return REINA;
+    }
+
+    public static int getREY() {
+        return REY;
+    }
+
+    
+    
+}
